13ae66dc95a41b7d123e1b52c88bbca620520aa9
Jerry Hsu <Jerry>
2021-09-14 15:36:33 -0400

    First Draft

--- src/Board.js
+++ src/Board.js
@@ -27,18 +27,33 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows=5, ncols=5, chanceLightStartsOn=0.5 }) {
   const [board, setBoard] = useState(createBoard());
+
+  function createTile(prob) {
+    const randNum = Math.random();
+    return (randNum < prob) ? true : false;
+  }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
-    let initialBoard = [];
     // TODO: create array-of-arrays of true/false values
+    let initialBoard = [];
+    for (let i=0; i<nrows; i++) {
+      let row = [];
+      for (let j=0; j<ncols; j++) {
+        row.push(createTile(chanceLightStartsOn));
+      }
+      initialBoard.push(row);
+    }
+
     return initialBoard;
   }
 
   function hasWon() {
     // TODO: check the board in state to determine whether the player has won.
+    const boardArr = board.flat();
+    return (boardArr.every( tile => tile === boardArr[0]));
   }
 
   function flipCellsAround(coord) {
@@ -55,17 +70,53 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
 
       // TODO: Make a (deep) copy of the oldBoard
 
+      let newBoard = [...oldBoard];
+
       // TODO: in the copy, flip this cell and the cells around it
+      let coordUpdate = [
+        {"y":y, "x": x},
+        {"y":y+1, "x":x},
+        {"y":y-1, "x":x},
+        {"y":y, "x":x+1},
+        {"y":y, "x":x-1}
+      ]
+      coordUpdate.forEach( ({y, x})=> {
+        flipCell(y,x,newBoard);
+      });
 
       // TODO: return the copy
+      setBoard(newBoard);
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
 
   // TODO
-
-  // make table board
+  if (hasWon()) {
+    return (
+      <div>
+        <h1>Congratulations, you have won!</h1>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        { board.forEach( (row) =>{
+          return (
+          <tr> 
+            {
+              row.forEach( (tile)=>{
+                <td>
+                  <Cell flipCellsAroundMe={flipCellsAround} isLit={tile} />
+                </td>;
+              })
+            }
+          </tr>
+          )
+        })}
+      </div>
+    )
+  }
 
   // TODO
 }
